8048d1d572898875698be605971bed259fbf1854
Abdullatif ALAJLAN <Tefo@Abdullatifs-MacBook-Pro.local>
2016-05-04 19:35:19 +1000

    few navigation fixes

---
+++ include/aboutpath.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tefo
+ * Date: 4/05/2016
+ * Time: 7:24 PM
+ */
+
+$home='"nav-item"';
+$ven='"nav-item"';
+$event='"nav-item"';
+$about='"active nav-item"';
+$navlogin='"nav-item"';
+$sign='"nav-item"';
+$url_home = '"index.php"' ;
+$url_ven = '"map/index.php"' ;
+$url_event_create = '"event/createEventFromOther.php" id="notlogedin"' ;
+$url_event_list = '"event/listEvent.php"' ;
+$url_about = '"about.php"' ;
+$url_login ='"user/index.php"';
+$url_profile ='"user/profile.php"';
+$url_sign ='"user/sign-up.php"';
+$url_log = "user/logout.php";
+
+?>
