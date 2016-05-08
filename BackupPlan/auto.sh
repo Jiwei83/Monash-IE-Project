f44f9cfab82b6ac8b2f4b3e6656defd989b526c0
Abdullatif ALAJLAN <Tefo@dyn-118-138-6-189.its.monash.edu.au>
2016-05-08 23:30:14 +1000

    recovery

---
+++ BackupPlan/auto.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -O ~/backup-output.txt -q --auth-no-challenge --http-user="acac1537" --http-password="Tefo0500500" https://cp152.ezyreg.com:2083/cpsess8322226416/frontend/paper_lantern/backup/dofullbackup.html --post-data="dest=homedir&email=fo8@hotmail.com"  
