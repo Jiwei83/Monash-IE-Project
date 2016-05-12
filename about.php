aba4e67b95fafedd995ab874069cdd5fdda3eb58
Lei Zhang <LeiZhang@dyn-118-139-30-145.its.monash.edu.au>
2016-05-12 15:03:02 +1000

    add about footer

--- about.php
+++ about.php
@@ -116,65 +116,27 @@ if($login->is_loggedin()) : ?>
         </div><!--//container-->
     </section><!--//story-video-->
     
-      <!-- ******FOOTER****** --> 
-    <footer class="footer">
-        <div class="footer-content">
-            <div class="container">
-                
-                <div class="row has-divider">
-                    <div class="footer-col download col-md-6 col-sm-12 col-xs-12">
-                        <div class="footer-col-inner">
-                            
-                        </div><!--//footer-col-inner-->
-                    </div><!--//download-->
-                    <div class="footer-col contact col-md-6 col-sm-12 col-xs-12">
-                        <div class="footer-col-inner">
-                            <h3 class="title">Contact us</h3>                          
-                            <p class="adr clearfix">
-                                <i class="fa fa-map-marker pull-left"></i>        
-                                <span class="adr-group pull-left">       
-                                    <span class="street-address">Monash University</span><br>
-                                    <span class="region">900 Dandenong Rd</span><br>
-                                    <span class="postal-code">Caulfield East VIC 3145</span><br>
-                                    <span class="country-name">Au</span>
-                                </span>
-                            </p>
-                            <p class="email"><i class="fa fa-envelope-o"></i><a href="#">enquires@active-family.net</a></p> 
-                            <a href="https://twitter.com/activeFamily4" class="twitter-follow-button" data-show-count="false">Follow @activeFamily4</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>                        
-                        </div><!--//footer-col-inner-->
-                    </div><!--//contact-->
-                </div>
-            </div><!--//container-->
-        </div><!--//footer-content-->
-        <div class="bottom-bar">
-            <div class="container">
-                <small class="copyright">Copyright @ 2016 <a href="copyright.txt" target="_blank">Active family</a></small>                
-            </div><!--//container-->
-        </div><!--//bottom-bar-->
-    </footer><!--//footer-->
-    
-    
+      <!-- ******FOOTER****** -->
+    <?php
 
+    include('include/footer.php');
 
+    ?>
 
-    
-    <!-- Javascript -->          
-    <script type="text/javascript" src="assets/plugins/jquery-1.11.2.min.js"></script>
-    <script type="text/javascript" src="assets/plugins/jquery-migrate-1.2.1.min.js"></script>
-    <script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script> 
-    <script type="text/javascript" src="assets/plugins/bootstrap-hover-dropdown.min.js"></script>
-    <script type="text/javascript" src="assets/plugins/back-to-top.js"></script>
-    <script type="text/javascript" src="assets/plugins/jquery-placeholder/jquery.placeholder.js"></script>
-    <script type="text/javascript" src="assets/plugins/FitVids/jquery.fitvids.js"></script>
-    <script type="text/javascript" src="assets/plugins/flexslider/jquery.flexslider-min.js"></script>     
-    <script type="text/javascript" src="assets/js/main.js"></script>
-    
-    <!-- Vimeo video API -->
-    <script src="http://a.vimeocdn.com/js/froogaloop2.min.js"></script>
-    <script type="text/javascript" src="assets/js/vimeo.js"></script> 
-    
-            
 </body>
+<!-- Javascript -->
+<script type="text/javascript" src="assets/plugins/jquery-1.11.2.min.js"></script>
+<script type="text/javascript" src="assets/plugins/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/plugins/bootstrap-hover-dropdown.min.js"></script>
+<script type="text/javascript" src="assets/plugins/back-to-top.js"></script>
+<script type="text/javascript" src="assets/plugins/jquery-placeholder/jquery.placeholder.js"></script>
+<script type="text/javascript" src="assets/plugins/FitVids/jquery.fitvids.js"></script>
+<script type="text/javascript" src="assets/plugins/flexslider/jquery.flexslider-min.js"></script>
+<script type="text/javascript" src="assets/js/main.js"></script>
+
+<!-- Vimeo video API -->
+<script src="http://a.vimeocdn.com/js/froogaloop2.min.js"></script>
+<script type="text/javascript" src="assets/js/vimeo.js"></script>
 </html> 
 
