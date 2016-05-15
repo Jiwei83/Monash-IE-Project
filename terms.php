3391fe7768c62b04b19aa9f29650a36dc282be25
Lei Zhang <LeiZhang@LeideMBP.gateway>
2016-05-15 21:46:10 +1000

    add footer

--- terms.php
+++ terms.php
@@ -137,42 +137,11 @@ if($login->is_loggedin()) : ?>
 </section><!--//story-video-->
 
 <!-- ******FOOTER****** -->
-<footer class="footer">
-    <div class="footer-content">
-        <div class="container">
+<?php
 
-            <div class="row has-divider">
-                <div class="footer-col download col-md-6 col-sm-12 col-xs-12">
-                    <div class="footer-col-inner">
+include('include/footer.php');
 
-                    </div><!--//footer-col-inner-->
-                </div><!--//download-->
-                <div class="footer-col contact col-md-6 col-sm-12 col-xs-12">
-                    <div class="footer-col-inner">
-                        <h3 class="title">Contact us</h3>
-                        <p class="adr clearfix">
-                            <i class="fa fa-map-marker pull-left"></i>
-                                <span class="adr-group pull-left">
-                                    <span class="street-address">Monash University</span><br>
-                                    <span class="region">900 Dandenong Rd</span><br>
-                                    <span class="postal-code">Caulfield East VIC 3145</span><br>
-                                    <span class="country-name">Au</span>
-                                </span>
-                        </p>
-                        <p class="email"><i class="fa fa-envelope-o"></i><a href="#">enquires@active-family.net</a></p>
-                        <a href="https://twitter.com/activeFamily4" class="twitter-follow-button" data-show-count="false">Follow @activeFamily4</a>
-                        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-                    </div><!--//footer-col-inner-->
-                </div><!--//contact-->
-            </div>
-        </div><!--//container-->
-    </div><!--//footer-content-->
-    <div class="bottom-bar">
-        <div class="container">
-            <small class="copyright">Copyright @ 2016 <a href="copyright.txt" target="_blank">Active family</a></small>
-        </div><!--//container-->
-    </div><!--//bottom-bar-->
-</footer><!--//footer-->
+?>
 
 
 
