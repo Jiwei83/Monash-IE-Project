f146c924c4277bbcc10873c1096901245260aa00
minor fixes <tefo@dyn-118-139-33-3.its.monash.edu.au>
2016-05-19 21:10:32 +1000

    compatibility issue

--- event/edit.php
+++ event/edit.php
@@ -1,6 +1,6 @@
 <?php
 include('../include/config.php');
-require_once('../class.user.php');
+//require_once('../class.user.php');
 include('../include/eventpath.php');
 include('../include/header.php');
 $eventId= $_GET['eventId'];
@@ -10,11 +10,10 @@ $eventId= $_GET['eventId'];
 
 
 ?>
-
+<link rel="stylesheet" href="style.css" type="text/css"  />
 
 <?php include('../include/navigation.php');?>
-<!-- ******Steps Section****** -->
-<section class="steps section">
+
 
     <div class="container">
         <form class="form-signin" method="post" id="login-form"
@@ -24,7 +23,7 @@ $eventId= $_GET['eventId'];
             <div class="form-group">
                 Title<span>*</span>
                 <input type="hidden" name="event_id" value="<?php echo $eventId ?>">
-                <input type="text" class="form-control" name="eTitle" value="<?php echo $list['eventName']; ?>" required />
+                <input type="text" class="form-control" name="eTitle" value="<?php echo $list['eventName']; ?>" required="true" />
                 <span id="check-e"></span>
             </div>
 
@@ -75,9 +74,16 @@ $eventId= $_GET['eventId'];
 
 
  </div>
-    <span id="check-e"></span>
-
+<!--    <span id="check-e"></span>-->
 </section>
+<script type="text/javascript"> var form = document.getElementById('login-form');
+    form.noValidate = true;
+    form.addEventListener('submit', function(event) { // listen for form submitting
+        if (!event.target.checkValidity()) {
+            event.preventDefault(); // dismiss the default functionality
+            alert('Please, fill all the required fields'); // error message
+        }
+    }, false);</script>
 <?php
 
 include('../include/footer.php');
@@ -87,13 +93,13 @@ include('../include/footer.php');
 <link rel="stylesheet" type="text/css" href="datetimepicker-master/jquery.datetimepicker.css"/ >
 <script src="datetimepicker-master/jquery.js"></script>
 <script src="datetimepicker-master/build/jquery.datetimepicker.full.min.js"></script>
-
-<!--<script type="text/javascript">-->
-<!--    document.getElementById("updateButton").onclick = function () {-->
-<!--        location.href = "../user/home.php";-->
-<!--    };-->
-<!---->
-<!--</script>-->
+<script type="text/javascript" src="assets/plugins/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/plugins/back-to-top.js"></script>
+<script type="text/javascript" src="assets/plugins/jquery-placeholder/jquery.placeholder.js"></script>
+<script type="text/javascript" src="assets/plugins/FitVids/jquery.fitvids.js"></script>
+<script type="text/javascript" src="assets/plugins/flexslider/jquery.flexslider-min.js"></script>
+<script type="text/javascript" src="assets/js/main.js"></script>
 
 </body>
 </html>
