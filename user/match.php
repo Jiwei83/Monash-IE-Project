83515898d97cf7a359d3c8878d56e5c92e618727
minor fixes <tefo@dyn-118-139-33-3.its.monash.edu.au>
2016-05-19 20:05:34 +1000

    mobile compatibility issues

--- user/match.php
+++ user/match.php
@@ -1,5 +1,6 @@
 <?php
 //include the database config file
+include('../include/userpath.php');
 include('../include/config.php');
 //set the user session
 require_once("session.php");
@@ -79,71 +80,16 @@ $stmt = $pdo->query($sql);
 $sql = "SELECT * FROM interest ORDER BY match_scale";
 $stmt = $pdo->query($sql);
 $interestRow = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
+$title= "Welcome ".$userRow['user_name']."  List of Matched Profiles";
+include('../include/header.php');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="shortcut icon" href="../favicon.ico">
-    <!-- Global CSS -->
-    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
-    <!-- Plugins CSS -->
-    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" href="assets/plugins/flexslider/flexslider.css">
-    <!-- Theme CSS -->
-    <link id="theme-style" rel="stylesheet" href="../map/assets/css/styles.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="css/custom.css"/>
+
     <link rel="stylesheet" href="style.css" type="text/css"  />
 
     <link rel="stylesheet" href="https://cdn.datatables.net/t/bs/jq-2.2.0,dt-1.10.11,r-2.0.2/datatables.min.css"/>
     <link rel="stylesheet" href="style.css" type="text/css"  />
-    <title>Welcome - <?php print($userRow['email']); ?></title>
-</head>
 
-<body style="background-color: #f5f5f5">
-<!-- ******HEADER****** -->
-<header id="header" class="header navbar-fixed-top" style="position: relative; background-color: white">
-    <div class="container">
-        <h1 class="logo">
-            <a href="../index.php"><img src="../logo.png" style="height:120px"></a>
-        </h1><!--//logo-->
-        <nav class="main-nav navbar-right" role="navigation">
-            <div class="navbar-header">
-                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button><!--nav-toggle-->
-            </div><!--navbar-header-->
-            <div id="navbar" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="nav-item"><a href="../index.php">Home</a></li>
-                    <li class="nav-item"><a href="../map/index.php">Venues</a></li>
-                    <li class="nav-item">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-delay="0" data-close-others="flase">
-                            Events</a>
-                        <ul class="dropdown-menu">
-                            <li><a href="../event/createEventFromOther.php" id="notlogedin"><span class="glyphicon glyphicon-calendar" ></span>&nbsp;Create an Event</a></li>
-                            <li><a href="../event/listEvent.php"><span class="glyphicon glyphicon-calendar"></span>&nbsp;View Events</a></li>
-                        </ul>
-                    </li>                    <li class="nav-item"><a href="../about.php">About Us</a></li>
-                    <li class="active nav-item dropdown" id="notlogedin">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-delay="0" data-close-others="flase">
-                            <span class="glyphicon glyphicon-user"></span>&nbsp;Hi' <?php echo $userRow['user_fname']; ?>&nbsp;<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="../user/profile.php"><span class="glyphicon glyphicon-user"></span>&nbsp;View Profile</a></li>
-                            <li><a href="../user/logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>
-                        </ul>
-                    </li>
-                </ul><!--nav-->
-            </div><!--navabr-collapse-->
-        </nav><!--main-nav-->
-    </div><!--container-->
-</header><!--header-->
+<?php include('../include/navigation.php');?>
 
 
 <div class="clearfix"></div>
@@ -235,7 +181,7 @@ $interestRow = $stmt->fetchAll(PDO::FETCH_ASSOC);
     </div>
 
 </div>
-
+</section>
 <?php
 include("../include/footer.php");
 ?>
--- user/profile.php
+++ user/profile.php
@@ -1,7 +1,9 @@
 <?php
 //set the user session
+
 require_once("session.php");
 //include the database config file
+include('../include/userpath.php');
 include('../include/config.php');
 require_once("class.user.php");
 //create the user object
@@ -13,43 +15,10 @@ $stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
 $stmt->execute(array(":user_id"=>$user_id));
 
 $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
-if($auth_user->is_loggedin()) : ?>
-    <style type="text/css">
-        #register {
-            display: none;
-        }
-        #info{
-            display: none;
-        }
-    </style>
+$title= "Welcome ".$userRow['user_name']." Update Profile";
+include('../include/header.php');
+ ?>
 
-<?php else: ?>
-
-    <style type="text/css">
-        #notlogedin {
-            display: none;
-        }
-        #create {
-            pointer-events: none;
-            cursor: default;
-        }
-    </style>
-<?php endif; ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="shortcut icon" href="../favicon.ico">
-    <!-- Global CSS -->
-    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
-    <!-- Plugins CSS -->
-    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" href="assets/plugins/flexslider/flexslider.css">
-    <!-- Theme CSS -->
-    <link id="theme-style" rel="stylesheet" href="../map/assets/css/styles.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="css/custom.css"/>
     <link rel="stylesheet" href="style.css" type="text/css"  />
 
 <!--    <script type="text/javascript" src="jquery-1.11.3-jquery.min.js"></script>-->
@@ -72,58 +41,13 @@ if($auth_user->is_loggedin()) : ?>
 
 
     <script type="text/javascript" src="js/tabpane.js"></script>
-</head>
-
-<body style="background-color: #f5f5f5">
-
-<header id="header" class="header navbar-fixed-top" style="position: relative; background-color: white">
-    <div class="container">
-        <h1 class="logo">
-            <a href="../index.php"><img src="../logo.png" style="height:120px"></a>
-        </h1><!--//logo-->
-        <nav class="main-nav navbar-right" role="navigation">
-            <div class="navbar-header">
-                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button><!--//nav-toggle-->
-            </div><!--//navbar-header-->
-            <div id="navbar-collapse" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li class="nav-item"><a href="../index.php">Home</a></li>
-                    <li class="nav-item"><a href="../map/index.php">Venues</a></li>
-                    <li class="nav-item">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-delay="0" data-close-others="flase">
-                            Events</a>
-                        <ul class="dropdown-menu">
-                            <li><a href="../event/createEventFromOther.php" id="notlogedin"><span class="glyphicon glyphicon-calendar" ></span>&nbsp;Create an Event</a></li>
-                            <li><a href="../event/listEvent.php"><span class="glyphicon glyphicon-calendar"></span>&nbsp;View Events</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item"><a href="../about.php">About Us</a></li>
-                    <li class="nav-item"><a href="index.php" id="register">Log in</a></li>
-                    <li class="nav-item nav-item-cta last"><a class="btn btn-cta btn-cta-secondary" href="sign-up.php" id="register">Sign Up Free</a></li>
-                    <li class="nav-item" id="notlogedin">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-delay="0" data-close-others="flase">
-                            <span class="glyphicon glyphicon-user"></span>&nbsp;Hi' <?php echo $userRow['user_name']; ?>&nbsp;<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span>&nbsp;View Profile</a></li>
-                            <li><a href="logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>
-                        </ul>
-                    </li>
-                </ul><!--//nav-->
-            </div><!--//navabr-collapse-->
-        </nav><!--//main-nav-->
-
-    </div><!--container-->
-</header><!--header-->
 
 
 
+<?php include('../include/navigation.php');?>
 
-    <div class="container-fluid" style="margin-top:80px;">
+
+<div class="container-fluid" style="margin-top:80px;">
 
     <div class="container">
     
@@ -210,6 +134,7 @@ if($auth_user->is_loggedin()) : ?>
     </div>
 
 </div>
+</section>
 <?php
 //update the user information
 if(isset($_POST['update'])) {
