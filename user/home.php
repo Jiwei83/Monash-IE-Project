9b0d74f852c385600b0234184734d890aa4d9e36
minor fixes <tefo@dyn-118-139-33-3.its.monash.edu.au>
2016-05-19 19:45:53 +1000

    mobile compatibility issue

--- user/home.php
+++ user/home.php
@@ -1,5 +1,7 @@
 <?php
+
     //include the database config file
+    include('../include/userpath.php');
     include('../include/config.php');
     //set the user session
 	require_once("session.php");
@@ -82,96 +84,22 @@
             //echo "Mail Error: " . $mail->ErrorInfo;
         }
     }
+$title= "Welcome ".$userRow['user_name'];
+include('../include/header.php');
 
-if($auth_user->is_loggedin()) : ?>
-    <style type="text/css">
-        #register {
-            display: none;
-        }
-        #info{
-            display: none;
-        }
-    </style>
-
-<?php else: ?>
-
-    <style type="text/css">
-        #notlogedin {
-            display: none;
-        }
-        #create {
-            pointer-events: none;
-            cursor: default;
-        }
-    </style>
-<?php endif; ?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="shortcut icon" href="../favicon.ico">
-
-    <!-- Global CSS -->
-    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
-    <!-- Plugins CSS -->
-    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.css">
-    <link rel="stylesheet" href="assets/plugins/flexslider/flexslider.css">
-    <!-- Theme CSS -->
-    <link id="theme-style" rel="stylesheet" href="../map/assets/css/styles.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+?>
     <link rel="stylesheet" href="css/custom.css"/>
     <link rel="stylesheet" href="style.css" type="text/css"  />
 
     <link rel="stylesheet" href="https://cdn.datatables.net/t/bs/jq-2.2.0,dt-1.10.11,r-2.0.2/datatables.min.css"/>
 <link rel="stylesheet" href="style.css" type="text/css"  />
 <title>Welcome - <?php print($userRow['user_email']); ?></title>
-</head>
 
-<body style="background-color: #f5f5f5">
-<!-- ******HEADER****** -->
-<header id="header" class="header navbar-fixed-top" style="position: relative; background-color: white">
-    <div class="container">
-        <h1 class="logo">
-            <a href="../index.php"><img src="../logo.png" style="height:120px"></a>
-        </h1><!--//logo-->
-        <nav class="main-nav navbar-right" role="navigation">
-            <div class="navbar-header">
-                <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button><!--//nav-toggle-->
-            </div><!--//navbar-header-->
-            <div id="navbar-collapse" class="navbar-collapse collapse">
-                <ul class="nav navbar-nav">
-                    <li class="nav-item"><a href="../index.php">Home</a></li>
-                    <li class="nav-item"><a href="../map/index.php">Venues</a></li>
-                    <li class="nav-item">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-delay="0" data-close-others="flase">
-                            Events</a>
-                        <ul class="dropdown-menu">
-                            <li><a href="../event/createEventFromOther.php" id="notlogedin"><span class="glyphicon glyphicon-calendar" ></span>&nbsp;Create an Event</a></li>
-                            <li><a href="../event/listEvent.php"><span class="glyphicon glyphicon-calendar"></span>&nbsp;View Events</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item"><a href="../about.php">About Us</a></li>
-                    <li class="nav-item"><a href="index.php" id="register">Log in</a></li>
-                    <li class="nav-item nav-item-cta last"><a class="btn btn-cta btn-cta-secondary" href="sign-up.php" id="register">Sign Up Free</a></li>
-                    <li class="nav-item" id="notlogedin">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-delay="0" data-close-others="flase">
-                            <span class="glyphicon glyphicon-user"></span>&nbsp;Hi' <?php echo $userRow['user_name']; ?>&nbsp;<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="profile.php"><span class="glyphicon glyphicon-user"></span>&nbsp;View Profile</a></li>
-                            <li><a href="logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>
-                        </ul>
-                    </li>
-                </ul><!--//nav-->
-            </div><!--//navabr-collapse-->
-        </nav><!--//main-nav-->
-    </div><!--container-->
-</header><!--header-->
+
+<?php
+
+
+include('../include/navigation.php'); ?>
 
 
     <div class="clearfix"></div>
@@ -293,7 +221,7 @@ if($auth_user->is_loggedin()) : ?>
     </div>
 
 </div>
-
+</section>
 <?php
 include("../include/footer.php");
 ?>
