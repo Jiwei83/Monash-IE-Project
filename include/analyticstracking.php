24e52ab36cf980a3ad709fa91d0806c443af049d
Abdullatif ALAJLAN <Tefo@Abdullatifs-MacBook-Pro.local>
2016-05-24 22:35:31 +1000

    google analitcs

---
+++ include/analyticstracking.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tefo
+ * Date: 24/05/2016
+ * Time: 10:32 PM
+ */
+?>
+
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-78252038-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
